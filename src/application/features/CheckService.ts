2b50fd6200fb365a34d184cd7cf9c7a9f5a0ce8d
Alexander Nole <cnole_azabache@outlook.com>
2023-12-03 08:11:15 -0500

    Implement dependency injection

--- src/application/features/CheckService.ts
+++ src/application/features/CheckService.ts
@@ -1,8 +1,11 @@
+import { ILogRepository, Log, LogSeverityLevel } from '../../domain';
+
 type SuccessCallback = () => void;
 type ErrorCallback = (error: string) => void;
 
 export class CheckService {
   constructor(
+    private readonly logRepository: ILogRepository,
     private readonly successCallback: SuccessCallback,
     private readonly errorCallback: ErrorCallback
   ) {}
@@ -12,8 +15,15 @@ export class CheckService {
       const res = await fetch(url);
       if (!res.ok) throw new Error(`Error on check service: ${url}`);
 
+      const log = new Log(`Service ${url} working`, LogSeverityLevel.LOW);
+      this.logRepository.save(log);
+
       this.successCallback();
     } catch (error) {
+      const errorMessage = `${url} is not ok. ${error}`;
+      const log = new Log(errorMessage, LogSeverityLevel.HIGH);
+      this.logRepository.save(log);
+
       this.errorCallback(`${error}`);
     }
   }
--- src/console/server.ts
+++ src/console/server.ts
@@ -1,14 +1,19 @@
 import { CheckService } from '../application/features';
+import { LogRepository } from '../infrastructure';
+import { LogFileSystemDatasource } from '../infrastructure/datasources';
 import { CronService } from './cron';
 
 export class Server {
   start(): void {
     console.log('Server started...');
+    const logFileSystemDatasource = new LogFileSystemDatasource();
 
     const cron = new CronService('*/5 * * * * *');
     cron.start(async () => {
-      const url = 'https://google.com';
+      const url = 'http://localhost:3000';
+      // const url = 'https://google.com';
       const checkService = new CheckService(
+        new LogRepository(logFileSystemDatasource),
         () => console.log(`${url} is ok`),
         (error) => console.error(error)
       );
