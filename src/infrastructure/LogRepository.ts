20e829145025e4412477d7d8d3280e3c517cb212
Alexander Nole <cnole_azabache@outlook.com>
2023-12-03 07:39:34 -0500

    Implement log repository

---
+++ src/infrastructure/LogRepository.ts
@@ -0,0 +1,22 @@
+import {
+  ILogDatasource,
+  ILogRepository,
+  Log,
+  LogSeverityLevel,
+} from '../domain';
+
+export class LogRepository implements ILogRepository {
+  private readonly _logDatasource: ILogDatasource;
+
+  constructor(logDatasource: ILogDatasource) {
+    this._logDatasource = logDatasource;
+  }
+
+  async save(log: Log): Promise<void> {
+    this._logDatasource.save(log);
+  }
+
+  async getAll(logSeverityLevel: LogSeverityLevel): Promise<Log[]> {
+    return await this._logDatasource.getAll(logSeverityLevel);
+  }
+}
